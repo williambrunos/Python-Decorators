1d0b593183e101faeb87f75158b38bbe77e2861e
William Bruno Sales de Paula Lima <williambrunosalesdepaulalima@gmail.com>
2023-02-10 09:12:27 -0300

    feat: adding lru cache decorator and building it from scratch

--- decorators/main.py
+++ decorators/main.py
@@ -38,3 +38,31 @@ def add_two_numbers(first, second):
 
 print('=== WRAPS ===')
 print(add_two_numbers.__name__, add_two_numbers.__doc__)
+
+# 3 - LRU cache -> Least Recently Used cache algorithm
+print('=== LRU Cache ===')
+
+import random
+import time
+from functools import lru_cache
+
+
+@lru_cache(maxsize=None)
+def heavy_processing(n):
+    sleep_time = n + random.random()
+    time.sleep(sleep_time)
+
+# first time
+heavy_processing(0)
+# CPU times: user 363 µs, sys: 727 µs, total: 1.09 ms
+# Wall time: 694 ms
+
+# second time
+heavy_processing(0)
+# CPU times: user 4 µs, sys: 0 ns, total: 4 µs
+# Wall time: 8.11 µs
+
+# third time
+heavy_processing(0)
+# CPU times: user 5 µs, sys: 1 µs, total: 6 µs
+# Wall time: 7.15 µs
